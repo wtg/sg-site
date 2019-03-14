c6e8f132870d749eca19abee3e3257da65646f80
Joseph Lyon <joeylyon1@gmail.com>
2019-03-14 14:23:11 -0400

    update config

--- wp-config.php
+++ wp-config.php
@@ -81,8 +81,7 @@ define('WP_DEBUG', true);
 // in some setups HTTP_X_FORWARDED_PROTO might contain 
 // a comma-separated list e.g. http,https
 // so check for https existence
-if (strpos($_SERVER['HTTP_X_FORWARDED_PROTO'], 'https') !== false)
-	   $_SERVER['HTTPS']='on';
+$_SERVER['HTTPS']='on';
 
 define('WP_HOME', 'https://sg.rpi.edu');
 define('WP_SITEURL', 'https://sg.rpi.edu');
